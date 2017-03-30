c102f76cd6abfbdaa30418308d57e5635c1d7445
Silver <rodrigo-prata@hotmail.com>
2017-03-30 17:54:15 -0300

    Splash Screen

---
+++ app/src/main/java/br/com/silver/plock/SplashActivity.java
@@ -0,0 +1,29 @@
+package br.com.silver.plock;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by silver on 3/30/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
